c6299568d2f65426bdfc7d7d1672843fd6500152
Miracle Chukwuma <chukwuma258@gmail.com>
2023-11-25 12:13:12 +0100

    add model to match data returned from the api

---
+++ models/models.go
@@ -0,0 +1,56 @@
+package models
+
+import "time"
+
+type AirQuality struct {
+	DateTime              time.Time             `json:"dateTime"`
+	RegionCode            string                `json:"regionCode"`
+	Indexes               []index               `json:"indexes"`
+	Pollutants            []pollutant           `json:"pollutants"`
+	HealthRecommendations healthRecommendations `json:"healthRecommendations"`
+}
+
+type index struct {
+	Code              string `json:"code"`
+	DisplayName       string `json:"displayName"`
+	Aqi               int16  `json:"aqi"`
+	AqiDisplay        string `json:"aqiDisplay"`
+	Color             color  `json:"color"`
+	Category          string `json:"category"`
+	DominantPollutant string `json:"dominantPollutant"`
+}
+
+type color struct {
+	Red   int8 `json:"red"`
+	Green int8 `json:"green"`
+	Blue  int8 `json:"blue"`
+	Alpha int8 `json:"alpha"`
+}
+
+type pollutant struct {
+	Code           string         `json:"code"`
+	DisplayName    string         `json:"displayName"`
+	FullName       string         `json:"fullName"`
+	Concentration  concentration  `json:"concentration"`
+	AdditionalInfo additionalInfo `json:"additionalInfo"`
+}
+
+type concentration struct {
+	Value float64 `json:"value"`
+	Units string  `json:"units"`
+}
+
+type additionalInfo struct {
+	Sources string `json:"sources"`
+	Effects string `json:"effects"`
+}
+
+type healthRecommendations struct {
+	GeneralPopulation      string `json:"generalPopulation"`
+	Elderly                string `json:"elderly"`
+	LungDiseasePopulation  string `json:"lungDiseasePopulation"`
+	HeartDiseasePopulation string `json:"heartDiseasePopulation"`
+	Athletes               string `json:"athletes"`
+	PregnantWomen          string `json:"pregnantWomen"`
+	Children               string `json:"children"`
+}
